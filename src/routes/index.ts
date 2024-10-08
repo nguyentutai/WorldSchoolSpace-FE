9f9e9e26e4edfb42004e02427778bcf2be2572ab
tai19062004 <nguyentutai1964@gmail.com>
2024-10-08 23:12:13 +0700

    feature/new-page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import config from "../config";
 import ClientLayout from "../layouts/ClientLayout";
 import Home from "../pages/Home/Home";
+import NewPage from "../pages/NewPage/NewPage";
 import Signin from "../pages/Signin/Signin";
 import Signup from "../pages/Signup/Signup";
 import Blogs from "./../pages/Blogs/Blogs";
@@ -10,4 +11,5 @@ export const publicRoutes = [
   { path: config.routes.Signin, layout: ClientLayout, element: Signin },
   { path: config.routes.Signup, layout: ClientLayout, element: Signup },
   { path: config.routes.Blogs, layout: ClientLayout, element: Blogs },
+  { path: config.routes.newBlog, layout: ClientLayout, element: NewPage },
 ];
